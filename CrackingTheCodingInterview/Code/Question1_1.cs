52d232936d923188e6921a79f08b61779f61e99b
Rob Keim <robkeim@gmail.com>
2016-03-13 23:19:14 +0100

    Add skeleton and question 1.1

---
+++ CrackingTheCodingInterview/Code/Question1_1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public class Question1_1
+    {
+
+        // 1.1 Implement an algorithm to determine if a string has all unique characters.  What if you cannot use additional data structures?
+
+        // Space: O(N)
+        // Time: O(N)
+        public static bool AreAllCharactersUnique(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return true;
+            }
+
+            HashSet<char> charactersInString = new HashSet<char>();
+            foreach (char c in input)
+            {
+                if (!charactersInString.Add(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // Space: O(1)
+        // Time: O(N^2)
+        public static bool AreAllCharactersUniqueNoAdditionalMemory(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return true;
+            }
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int j = i; j < input.Length; j++)
+                {
+                    if (i != j && input[i] == input[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/Test1_1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Test1_1
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            // No duplicates
+            Assert.IsTrue(Question1_1.AreAllCharactersUnique("abc"));
+            Assert.IsTrue(Question1_1.AreAllCharactersUniqueNoAdditionalMemory("abc"));
+
+            // Duplicates
+            Assert.IsFalse(Question1_1.AreAllCharactersUnique("aba"));
+            Assert.IsFalse(Question1_1.AreAllCharactersUniqueNoAdditionalMemory("aba"));
+        }
+
+        [TestMethod]
+        public void CaseSensitivityTest()
+        {
+            // 'A' and 'a' are considered different characters
+            Assert.IsTrue(Question1_1.AreAllCharactersUnique("Aa"));
+            Assert.IsTrue(Question1_1.AreAllCharactersUniqueNoAdditionalMemory("Aa"));
+        }
+
+        [TestMethod]
+        public void NullAndEmptyStringsTest()
+        {
+            // Null string
+            Assert.IsTrue(Question1_1.AreAllCharactersUnique(null));
+            Assert.IsTrue(Question1_1.AreAllCharactersUniqueNoAdditionalMemory(null));
+
+            // Empty string
+            Assert.IsTrue(Question1_1.AreAllCharactersUnique(string.Empty));
+            Assert.IsTrue(Question1_1.AreAllCharactersUniqueNoAdditionalMemory(string.Empty));
+        }
+    }
+}
