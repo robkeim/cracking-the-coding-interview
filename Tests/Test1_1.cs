8313785159cca383dc13d64f65d49c21cf5f3ed9
Rob Keim <robkeim@gmail.com>
2016-03-13 23:30:24 +0100

    Refactor tests for 1.1

--- Tests/Test1_1.cs
+++ Tests/Test1_1.cs
@@ -11,32 +11,30 @@ namespace Tests
         public void BasicTest()
         {
             // No duplicates
-            Assert.IsTrue(Question1_1.AreAllCharactersUnique("abc"));
-            Assert.IsTrue(Question1_1.AreAllCharactersUniqueNoAdditionalMemory("abc"));
+            ValidateInput("abc", true);
 
             // Duplicates
-            Assert.IsFalse(Question1_1.AreAllCharactersUnique("aba"));
-            Assert.IsFalse(Question1_1.AreAllCharactersUniqueNoAdditionalMemory("aba"));
+            ValidateInput("aba", false);
         }
 
         [TestMethod]
         public void CaseSensitivityTest()
         {
             // 'A' and 'a' are considered different characters
-            Assert.IsTrue(Question1_1.AreAllCharactersUnique("Aa"));
-            Assert.IsTrue(Question1_1.AreAllCharactersUniqueNoAdditionalMemory("Aa"));
+            ValidateInput("Aa", true);
         }
 
         [TestMethod]
         public void NullAndEmptyStringsTest()
         {
-            // Null string
-            Assert.IsTrue(Question1_1.AreAllCharactersUnique(null));
-            Assert.IsTrue(Question1_1.AreAllCharactersUniqueNoAdditionalMemory(null));
+            ValidateInput(null, true);
+            ValidateInput(string.Empty, true);
+        }
 
-            // Empty string
-            Assert.IsTrue(Question1_1.AreAllCharactersUnique(string.Empty));
-            Assert.IsTrue(Question1_1.AreAllCharactersUniqueNoAdditionalMemory(string.Empty));
+        private void ValidateInput(string input, bool expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Question1_1.AreAllCharactersUnique(string.Empty));
+            Assert.AreEqual(expectedResult, Question1_1.AreAllCharactersUniqueNoAdditionalMemory(string.Empty));
         }
     }
 }
