d91beba5c5c5add3eee3c24677f85dcd1f1b49d6
Rob Keim <robkeim@gmail.com>
2016-03-14 22:25:10 +0100

    Add 1.6

---
+++ CrackingTheCodingInterview/Code/Question1_6.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Code
+{
+    public static class Question1_6
+    {
+        // 1.6 Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+
+        // Space: O(N^2)
+        // Time: O(N^2)
+        public static int[,] RotateMatrix(int[,] matrix)
+        {
+            if (matrix == null)
+            {
+                throw new ArgumentNullException(nameof(matrix));
+            }
+
+            if (matrix.Rank != 2 || !IsPerfectSquare(matrix.Length))
+            {
+                throw new ArgumentException(nameof(matrix), "Matrix needs to be a two dimensional square");
+            }
+
+            var size = (int)Math.Sqrt(matrix.Length);
+            var result = new int[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    result[col, size - 1 - row] = matrix[row, col];
+                }
+            }
+
+            return result;
+        }
+
+        // Space: O(1)
+        // Time: O(N^2)
+        public static void RotateMatrixInPlace(int[,] matrix)
+        {
+            if (matrix == null)
+            {
+                throw new ArgumentNullException(nameof(matrix));
+            }
+            
+            if (matrix.Rank != 2 || !IsPerfectSquare(matrix.Length))
+            {
+                throw new ArgumentException(nameof(matrix), "Matrix needs to be a two dimensional square");
+            }
+
+            var size = (int)Math.Sqrt(matrix.Length);
+            var offset = 0;
+
+            while (size > 1)
+            {
+                for (int i = 0; i < size - 1; i++)
+                {
+                    var orig = matrix[offset, i + offset];
+
+                    // Top left
+                    matrix[offset, i + offset] = matrix[size - 1 - i + offset, offset];
+
+                    // Bottom left
+                    matrix[size - 1 - i + offset, offset] = matrix[size - 1 + offset, size - 1 - i + offset];
+
+                    // Bottom right
+                    matrix[size - 1 + offset, size - 1 - i + offset] = matrix[i + offset, size - 1 + offset];
+
+                    // Top right
+                    matrix[i + offset, size - 1 + offset] = orig;
+                }
+
+                size -= 2;
+                offset++;
+            }
+        }
+
+        private static bool IsPerfectSquare(int n)
+        {
+            return Math.Sqrt(n) % 1 == 0;
+        }
+    }
+}
---
+++ Tests/Test1_6.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Test1_6
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            // 01 02 03 04 05
+            // 06 07 08 09 10
+            // 11 12 13 14 15
+            // 16 17 18 19 20
+            // 21 22 23 24 25
+
+            // 2x2
+            var input = GenerateMatrix(1, 2, 3, 4);
+            var expectedResult = GenerateMatrix(3, 1, 4, 2);
+            ValidateResult(input, expectedResult);
+
+            // 3x3
+            input = GenerateMatrix(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            expectedResult = GenerateMatrix(7, 4, 1, 8, 5, 2, 9, 6, 3);
+            ValidateResult(input, expectedResult);
+
+            // 4x4
+            input = GenerateMatrix(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+            expectedResult = GenerateMatrix(13, 9, 5, 1, 14, 10, 6, 2, 15, 11, 7, 3, 16, 12, 8, 4);
+            ValidateResult(input, expectedResult);
+
+            // 5x5
+            input = GenerateMatrix(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25);
+            expectedResult = GenerateMatrix(21, 16, 11, 6, 1, 22, 17, 12, 7, 2, 23, 18, 13, 8, 3, 24, 19, 14, 9, 4, 25, 20, 15, 10, 5);
+            ValidateResult(input, expectedResult);
+        }
+
+        [TestMethod]
+        public void EdgeCaseTest()
+        {
+            // 1x1
+            var input = GenerateMatrix(1);
+            var expectedResult = GenerateMatrix(1);
+            ValidateResult(input, expectedResult);
+        }
+        
+        [TestMethod]
+        public void InvalidInputsTest()
+        {
+            // Null matrix
+            TestHelpers.AssertExceptionThrown(() => { Question1_6.RotateMatrix(null); }, typeof(ArgumentNullException));
+            TestHelpers.AssertExceptionThrown(() => { Question1_6.RotateMatrixInPlace(null); }, typeof(ArgumentNullException));
+        }
+
+        private void ValidateResult(int[,] input, int[,] expectedResult)
+        {
+            var size = (int)Math.Sqrt(input.Length);
+
+            var result1 = new int[size, size];
+            var result2 = new int[size, size];
+
+            // Perform deep-copies of the original array
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    result1[i, j] = input[i, j];
+                    result2[i, j] = input[i, j];
+                }
+            }
+
+            result1 = Question1_6.RotateMatrix(result1);
+            Question1_6.RotateMatrixInPlace(result2);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Assert.AreEqual(expectedResult[i, j], result1[i, j]);
+                    Assert.AreEqual(expectedResult[i, j], result2[i, j]);
+                }
+            }
+        }
+        
+        // Generates a two dimensional matrix from a one dimensional input
+        // Example input:
+        // 1 2 3 4 5 6 7 8 9
+        // Example output:
+        // 1 2 3
+        // 4 5 6
+        // 7 8 9
+        private int[,] GenerateMatrix(params int[] list)
+        {
+            if (!IsPerfectSquare(list.Length))
+            {
+                throw new ArgumentException(nameof(list), "Number of elements must be a perfect square to create an NxN matrix");
+            }
+
+            var size = (int)Math.Sqrt(list.Length);
+
+            var result = new int[size, size];
+            var counter = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    result[i, j] = list[counter++];
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsPerfectSquare(int n)
+        {
+            return Math.Sqrt(n) % 1 == 0;
+        }
+    }
+}
