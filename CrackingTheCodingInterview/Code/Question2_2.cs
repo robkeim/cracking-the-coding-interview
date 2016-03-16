04e83d187e4255aec3d12272db876e8425d7cafd
Rob Keim <robkeim@gmail.com>
2016-03-16 21:29:53 +0100

    Add 2.2

---
+++ CrackingTheCodingInterview/Code/Question2_2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Code
+{
+    public static class Question2_2
+    {
+        public static Node<T> FindKthToLast<T>(Node<T> head, int k) where T : IEquatable<T>
+        {
+            if (head == null)
+            {
+                throw new ArgumentNullException(nameof(head));
+            }
+
+            if (k < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(k), "Value must be greater than or equal to zero");
+            }
+
+            var result = head;
+
+            for (int i = 0; i < k; i++)
+            {
+                head = head.Next;
+
+                if (head == null)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(k), "There must be at least k elements in the list");
+                }
+            }
+
+            while (head.Next != null)
+            {
+                head = head.Next;
+                result = result.Next;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tests/Test2_2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Test2_2
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            const int NumElements = 5;
+            
+            var list = TestHelpers.CreateLinkedList(Enumerable.Range(0, NumElements).Reverse().ToArray());
+
+            for (int i = 0; i < NumElements; i++)
+            {
+                var input = TestHelpers.CloneList(list);
+                var result = Question2_2.FindKthToLast(input, i);
+
+                Assert.AreEqual(i, result.Data);
+            }           
+        }
+
+        [TestMethod]
+        public void InvalidInputsTest()
+        {
+            // Null input
+            TestHelpers.AssertExceptionThrown(() => { Question2_2.FindKthToLast<int>(null, 0); }, typeof(ArgumentNullException));
+
+            // K invalid
+            var list = TestHelpers.CreateLinkedList(1, 2, 3);
+            TestHelpers.AssertExceptionThrown(() => { Question2_2.FindKthToLast(list, -1); }, typeof(ArgumentOutOfRangeException));
+
+            list = TestHelpers.CreateLinkedList(1, 2, 3);
+            TestHelpers.AssertExceptionThrown(() => { Question2_2.FindKthToLast(list, 4); }, typeof(ArgumentOutOfRangeException));
+        }
+    }
+}
