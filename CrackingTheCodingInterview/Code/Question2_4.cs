17fda7ef32b84806276f17073c8cd719d9337b8d
Rob Keim <robkeim@gmail.com>
2016-03-22 23:37:10 +0100

    Finish 2.4 (partition)

--- CrackingTheCodingInterview/Code/Question2_4.cs
+++ CrackingTheCodingInterview/Code/Question2_4.cs
@@ -1,13 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Code
 {
     public static class Question2_4
     {
         // 2.4 Partition: Write code to parition a linked list around a value x, such that all nodes less than x come before all nodes greater than or equal to x.
+
+        // Space: O(1)
+        // Time: O(N)
+        // This solution is stable meaning that items less than and greater than/equal to x keep their relative order
+        public static Node<T> PartitionList<T>(Node<T> head, T x) where T : IEquatable<T>, IComparable<T>
+        {
+            if (head == null)
+            {
+                throw new ArgumentNullException(nameof(head));
+            }
+
+            Node<T> smallerHead = null;
+            Node<T> smallerLast = null;
+            Node<T> biggerHead = null;
+            Node<T> biggerLast = null;
+
+            while (head != null)
+            {
+                var nodeToAdd = new Node<T>(head.Data);
+
+                if (head.Data.CompareTo(x) < 0)
+                {
+                    if (smallerLast == null)
+                    {
+                        smallerHead = nodeToAdd;
+                        smallerLast = nodeToAdd;
+                    }
+                    else
+                    {
+                        smallerLast.Next = nodeToAdd;
+                        smallerLast = nodeToAdd;
+                    }
+                }
+                else
+                {
+                    if (biggerLast == null)
+                    {
+                        biggerHead = nodeToAdd;
+                        biggerLast = nodeToAdd;
+                    }
+                    else
+                    {
+                        biggerLast.Next = nodeToAdd;
+                        biggerLast = nodeToAdd;
+                    }
+                }
+
+                head = head.Next;
+            }
+
+            if (smallerHead == null)
+            {
+                return biggerHead;
+            }
+            else
+            {
+                smallerLast.Next = biggerHead;
+                return smallerHead;
+            }
+        }
     }
 }
---
+++ Tests/Test2_4.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Test2_4
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            var list = TestHelpers.CreateLinkedList(3, 2, 1);
+            ValidateResult(2, list, 1, 3, 2);
+
+            // No change needed (x is smaller)
+            list = TestHelpers.CreateLinkedList(1, 2, 3);
+            ValidateResult(0, list, 1, 2, 3);
+
+            // No change needed (x is larger)
+            list = TestHelpers.CreateLinkedList(1, 2, 3);
+            ValidateResult(4, list, 1, 2, 3);
+
+            list = TestHelpers.CreateLinkedList(1, 3, 3, 1);
+            ValidateResult(2, list, 1, 1, 3, 3);
+        }
+
+        [TestMethod]
+        public void EdgeCasesTest()
+        {
+            // One node
+            var list = TestHelpers.CreateLinkedList(1);
+            ValidateResult(0, list, 1);
+
+            list = TestHelpers.CreateLinkedList(1);
+            ValidateResult(1, list, 1);
+
+            list = TestHelpers.CreateLinkedList(1);
+            ValidateResult(2, list, 1);
+        }
+
+        [TestMethod]
+        public void InvalidInputsTest()
+        {
+            // Null node
+            TestHelpers.AssertExceptionThrown(() => { Question2_4.PartitionList(null, 3); }, typeof(ArgumentNullException));
+        }
+
+        private void ValidateResult<T>(T x, Node<T> list, params T[] expectedResult) where T : IEquatable<T>, IComparable<T>
+        {
+            var result = Question2_4.PartitionList(list, x);
+            TestHelpers.ValidateLinkedListContent(result, expectedResult);
+        }
+    }
+}
