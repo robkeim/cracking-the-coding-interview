959d7c9af2238c9ab7dfce603287e70864834e17
Rob Keim <robkeim@gmail.com>
2016-03-26 13:57:22 +0100

    Add Digit class/tests

---
+++ CrackingTheCodingInterview/Code/Digit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Code
+{
+    public class Digit
+    {
+        private readonly int value;
+
+        public Digit(int value)
+        {
+            if (value < 0 || value > 9)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "Value must be between 0 and 9 inclusive");
+            }
+
+            this.value = value;
+        }
+
+        public static implicit operator int(Digit d)
+        {
+            return d.value;
+        }
+    }
+}
---
+++ Tests/DigitTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class DigitTests
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var digit = new Digit(i);
+                Assert.AreEqual(i, digit);
+            }
+        }
+
+        [TestMethod]
+        public void InvalidInputTest()
+        {
+            // Negative number
+            TestHelpers.AssertExceptionThrown(() => { var digit = new Digit(-1); }, typeof(ArgumentOutOfRangeException));
+
+            // Multiple digit number
+            TestHelpers.AssertExceptionThrown(() => { var digit = new Digit(10); }, typeof(ArgumentOutOfRangeException));
+        }
+    }
+}
