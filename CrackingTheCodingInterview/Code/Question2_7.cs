13045e2ed4b7ef05ac2f53dca821ccb46f2a3305
Rob Keim <robkeim@gmail.com>
2016-03-26 20:24:15 +0100

    Add 2.7 (intersection)

---
+++ CrackingTheCodingInterview/Code/Question2_7.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Code
+{
+    public static class Question2_7
+    {
+        // 2.7 Intersection: Given two (singly) linked lists, determine if the two lists intersect.  Return th eintersection node.  Node that the intersection is defined based on reference, not value.  That is if the kth node of the first linked list is exactly the same node (by reference) as the jth node of the second linked list, then they are intersecting.
+        public static Node<T> FindIntersection<T>(Node<T> list1, Node<T> list2)
+            where T : IEquatable<T>
+        {
+            if (list1 == null)
+            {
+                throw new ArgumentNullException(nameof(list1));
+            }
+
+            if (list2 == null)
+            {
+                throw new ArgumentNullException(nameof(list2));
+            }
+
+            var list1Length = GetLength(list1);
+            var list2Length = GetLength(list2);
+
+            if (list1Length > list2Length)
+            {
+                for (int i = 0; i < list1Length - list2Length; i++)
+                {
+                    list1 = list1.Next;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < list2Length - list1Length; i++)
+                {
+                    list2 = list2.Next;
+                }
+            }
+
+            while (list1 != null)
+            {
+                if (ReferenceEquals(list1, list2))
+                {
+                    return list1;
+                }
+
+                list1 = list1.Next;
+                list2 = list2.Next;
+            }
+
+            // If not finding an intersection is a valid case then it makes sense to return null here,
+            // otherwise if we always expected an intersection, it would make more sense to throw an exception.
+            return null;
+        }
+
+        private static int GetLength<T>(Node<T> head)
+            where T : IEquatable<T>
+        {
+            if (head == null)
+            {
+                throw new ArgumentNullException(nameof(head));
+            }
+
+            int length = 0;
+
+            while (head != null)
+            {
+                length++;
+                head = head.Next;
+            }
+
+            return length;
+        }
+    }
+}
---
+++ Tests/Test2_7.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Test2_7
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            var list1 = TestHelpers.CreateLinkedList(1, 2, 3);
+            var list2 = TestHelpers.CreateLinkedList(4, 5, 6);
+            var intersection = TestHelpers.CreateLinkedList(7, 8, 9);
+            AddIntersection(list1, list2, intersection);
+            Validate(list1, list2, intersection);
+
+            // Different sized lists
+            list1 = TestHelpers.CreateLinkedList(1);
+            list2 = TestHelpers.CreateLinkedList(2, 3);
+            intersection = TestHelpers.CreateLinkedList(4, 5, 6);
+            AddIntersection(list1, list2, intersection);
+            Validate(list1, list2, intersection);
+
+            // Intersection is last node
+            list1 = TestHelpers.CreateLinkedList(1, 2);
+            list2 = TestHelpers.CreateLinkedList(3, 4, 5);
+            intersection = TestHelpers.CreateLinkedList(6);
+            AddIntersection(list1, list2, intersection);
+            Validate(list1, list2, intersection);
+
+            // Repeated values (to ensure references are being computed)
+            list1 = TestHelpers.CreateLinkedList(1, 3);
+            list2 = TestHelpers.CreateLinkedList(2, 2, 2);
+            intersection = TestHelpers.CreateLinkedList(2);
+            AddIntersection(list1, list2, intersection);
+            Validate(list1, list2, intersection);
+        }
+
+        [TestMethod]
+        public void InvalidInputsTest()
+        {
+            var head = new Node<int>(1);
+
+            TestHelpers.AssertExceptionThrown(() => Question2_7.FindIntersection(null, head), typeof(ArgumentNullException));
+            TestHelpers.AssertExceptionThrown(() => Question2_7.FindIntersection(head, null), typeof(ArgumentNullException));
+            TestHelpers.AssertExceptionThrown(() => Question2_7.FindIntersection<int>(null, null), typeof(ArgumentNullException));
+        }
+
+        private static void AddIntersection<T>(Node<T> list1, Node<T> list2, Node<T> intersection)
+            where T : IEquatable<T>
+        {
+            if (list1 == null)
+            {
+                throw new ArgumentNullException(nameof(list1));
+            }
+
+            if (list2 == null)
+            {
+                throw new ArgumentNullException(nameof(list2));
+            }
+
+            if (intersection == null)
+            {
+                throw new ArgumentNullException(nameof(intersection));
+            }
+
+            while (list1.Next != null)
+            {
+                list1 = list1.Next;
+            }
+
+            list1.Next = intersection;
+
+            while (list2.Next != null)
+            {
+                list2 = list2.Next;
+            }
+
+            list2.Next = intersection;
+        }
+
+        private static void Validate<T>(Node<T> list1, Node<T> list2, Node<T> intersection)
+            where T : IEquatable<T>
+        {
+            if (list1 == null)
+            {
+                throw new ArgumentNullException(nameof(list1));
+            }
+
+            if (list2 == null)
+            {
+                throw new ArgumentNullException(nameof(list2));
+            }
+
+            Assert.IsTrue(ReferenceEquals(intersection, Question2_7.FindIntersection(list1, list2)));
+        }
+    }
+}
