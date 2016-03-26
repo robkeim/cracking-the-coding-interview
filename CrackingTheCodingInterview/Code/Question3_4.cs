f703bf16926f32b1c4f176cc0efed86d0306249b
Rob Keim <robkeim@gmail.com>
2016-03-26 23:42:00 +0100

    Add 3.4 (queue via stacks)

---
+++ CrackingTheCodingInterview/Code/Question3_4.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Code
+{
+    // 3.4 Queue via Stacks: Implement a MyQueue class which implements a queue using two stacks.
+    [SuppressMessage("Documentation Rules", "SA1649", Justification = "Aligning with question naming convention")]
+    [SuppressMessage("Microsoft.Naming", "CA1711", Justification = "Using the name from the question")]
+    public class MyQueue<T>
+    {
+        private readonly Stack<T> stackStack;
+        private readonly Stack<T> queueStack;
+        private bool isQueue;
+        private int numItems;
+
+        public MyQueue()
+        {
+            stackStack = new Stack<T>();
+            queueStack = new Stack<T>();
+        }
+
+        public void Add(T item)
+        {
+            if (isQueue)
+            {
+                isQueue = false;
+                while (queueStack.Count != 0)
+                {
+                    stackStack.Push(queueStack.Pop());
+                }
+
+                stackStack.Push(item);
+            }
+            else
+            {
+                stackStack.Push(item);
+            }
+
+            numItems++;
+        }
+
+        public T Remove()
+        {
+            if (numItems == 0)
+            {
+                throw new InvalidOperationException("No items in the queue");
+            }
+
+            numItems--;
+
+            if (isQueue)
+            {
+                return queueStack.Pop();
+            }
+            else
+            {
+                isQueue = true;
+                while (stackStack.Count != 0)
+                {
+                    queueStack.Push(stackStack.Pop());
+                }
+
+                return queueStack.Pop();
+            }
+        }
+    }
+}
---
+++ Tests/Test3_4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Test3_4
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            var queue = new MyQueue<int>();
+
+            queue.Add(1);
+            queue.Add(2);
+
+            Assert.AreEqual(1, queue.Remove());
+            Assert.AreEqual(2, queue.Remove());
+
+            queue.Add(3);
+            Assert.AreEqual(3, queue.Remove());
+
+            queue.Add(4);
+            queue.Add(5);
+
+            Assert.AreEqual(4, queue.Remove());
+            Assert.AreEqual(5, queue.Remove());
+        }
+
+        [TestMethod]
+        public void InvalidOperationsTest()
+        {
+            var queue = new MyQueue<int>();
+
+            TestHelpers.AssertExceptionThrown(() => queue.Remove(), typeof(InvalidOperationException));
+        }
+    }
+}
