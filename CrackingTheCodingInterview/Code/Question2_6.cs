76f11069d1f6d5af6ba7db21733f35291b303c29
Rob Keim <robkeim@gmail.com>
2016-03-26 19:15:25 +0100

    Add 2.6 (palindrome)

---
+++ CrackingTheCodingInterview/Code/Question2_6.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Code
+{
+    public static class Question2_6
+    {
+        // 2.6 Palindrome: Implement a funciton to check if a linked lits is a palindrome.
+
+        // Space: O(N)
+        // Time: O(N)
+        public static bool IsPalindrome<T>(Node<T> head)
+            where T : IEquatable<T>
+        {
+            if (head == null)
+            {
+                throw new ArgumentNullException(nameof(head));
+            }
+
+            var reverse = ReverseAndClone(head);
+
+            while (head != null && reverse != null)
+            {
+                if (!head.Data.Equals(reverse.Data))
+                {
+                    return false;
+                }
+
+                head = head.Next;
+                reverse = reverse.Next;
+            }
+
+            return head == null && reverse == null;
+        }
+
+        private static Node<T> ReverseAndClone<T>(Node<T> head)
+            where T : IEquatable<T>
+        {
+            if (head == null)
+            {
+                throw new ArgumentNullException(nameof(head));
+            }
+
+            Node<T> next = null;
+
+            while (head != null)
+            {
+                var tmp = new Node<T>(head.Data);
+                tmp.Next = next;
+                next = tmp;
+
+                head = head.Next;
+            }
+
+            return next;
+        }
+    }
+}
---
+++ Tests/Test2_6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Test2_6
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            Validate(true, 1);
+            Validate(true, 1, 1);
+            Validate(true, 1, 2, 1);
+            Validate(true, 1, 2, 2, 1);
+
+            Validate(false, 1, 2);
+            Validate(false, 1, 2, 3);
+            Validate(false, 1, 2, 1, 2);
+        }
+
+        [TestMethod]
+        public void InvalidInputTest()
+        {
+            TestHelpers.AssertExceptionThrown(() => { Question2_6.IsPalindrome<int>(null); }, typeof(ArgumentNullException));
+        }
+
+        private static void Validate(bool expectedResult, params int[] values)
+        {
+            var head = TestHelpers.CreateLinkedList(values);
+            Assert.AreEqual(expectedResult, Question2_6.IsPalindrome(head));
+        }
+    }
+}
