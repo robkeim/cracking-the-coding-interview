f985d4a231fcb3f6f0351039dbda0cdfcaef61db
Rob Keim <robkeim@gmail.com>
2016-03-26 22:33:03 +0100

    Add 3.2 (stack min)

---
+++ CrackingTheCodingInterview/Code/Question3_2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Code
+{
+    // 3.2 Stack Min: How would you design a stack which, in addition to push and pop, has a function min which returns the minimum element?
+    //     Push, pop, and min should all operate in O(1) time.
+    [SuppressMessage("Documentation Rules", "SA1649", Justification = "Aligning with question naming convention")]
+    [SuppressMessage("Microsoft.Naming", "CA1711", Justification = "Explicitly not inheriting from stack")]
+    public class MinStack<T>
+        where T : IComparable<T>
+    {
+        // NOTE: a better implemention would grow the size dynamically when more space was needed
+        private const int MaxSize = 100;
+
+        private readonly T[] stackValues;
+        private readonly T[] minValues;
+
+        private int numElements;
+
+        public MinStack()
+        {
+            stackValues = new T[MaxSize];
+            minValues = new T[MaxSize];
+        }
+
+        public T Pop()
+        {
+            if (numElements == 0)
+            {
+                throw new InvalidOperationException("Stack does not contain any elements");
+            }
+
+            var result = stackValues[numElements - 1];
+            numElements--;
+
+            return result;
+        }
+
+        public void Push(T item)
+        {
+            if (numElements == MaxSize)
+            {
+                throw new InvalidOperationException("No more space in the stack");
+            }
+
+            stackValues[numElements] = item;
+
+            var curMin = numElements == 0
+                ? item
+                : minValues[numElements - 1];
+
+            minValues[numElements] = item.CompareTo(curMin) < 0
+                ? item
+                : curMin;
+
+            numElements++;
+        }
+
+        public T Min()
+        {
+            if (numElements == 0)
+            {
+                throw new InvalidOperationException("Stack does not contain any elements");
+            }
+
+            return minValues[numElements - 1];
+        }
+    }
+}
---
+++ Tests/Test3_2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Test3_2
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            var stack = new MinStack<int>();
+
+            // Build stack
+            PushAndValidateMin(stack, 1, 1);
+            PushAndValidateMin(stack, 2, 1);
+
+            // Empty stack
+            PopAndValidateMin(stack, 2, 1);
+            Assert.AreEqual(1, stack.Pop());
+
+            // Build stack
+            PushAndValidateMin(stack, 3, 3);
+            PushAndValidateMin(stack, 2, 2);
+            PushAndValidateMin(stack, 4, 2);
+            PushAndValidateMin(stack, 1, 1);
+
+            // Empty stack
+            PopAndValidateMin(stack, 1, 2);
+            PopAndValidateMin(stack, 4, 2);
+            PopAndValidateMin(stack, 2, 3);
+            Assert.AreEqual(3, stack.Pop());
+        }
+
+        [TestMethod]
+        public void InvalidOperationsTest()
+        {
+            var stack = new MinStack<int>();
+
+            TestHelpers.AssertExceptionThrown(() => stack.Min(), typeof(InvalidOperationException));
+            TestHelpers.AssertExceptionThrown(() => stack.Pop(), typeof(InvalidOperationException));
+        }
+
+        private static void PushAndValidateMin<T>(MinStack<T> stack, T item, T minAfterPush)
+            where T : IComparable<T>
+        {
+            stack.Push(item);
+            Assert.AreEqual(minAfterPush, stack.Min());
+        }
+
+        private static void PopAndValidateMin<T>(MinStack<T> stack, T poppedItem, T minAfterPop)
+            where T : IComparable<T>
+        {
+            Assert.AreEqual(poppedItem, stack.Pop());
+            Assert.AreEqual(minAfterPop, stack.Min());
+        }
+    }
+}
