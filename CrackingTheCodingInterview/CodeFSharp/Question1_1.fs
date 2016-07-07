1bba7bcfe3b70e5b02d907f1f97e152e29a5584b
Rob Keim <robkeim@gmail.com>
2016-07-07 23:30:17 +0200

    Add F# solution for 1.1 (is unique)

---
+++ CrackingTheCodingInterview/CodeFSharp/Question1_1.fs
@@ -0,0 +1,14 @@
+﻿namespace CodeFSharp
+
+// 1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters.
+// What if you cannot use additional data structures?
+module Question1_1 =
+    let private CountUniqueCharacters string =
+        string
+            |> Seq.distinct
+            |> Seq.length
+
+    // Space: O(N)
+    // Time: O(N)
+    let AreAllCharactersUnique string =
+        Seq.length string = CountUniqueCharacters string   
---
+++ CrackingTheCodingInterview/TestsFSharp/Test1_1.fs
@@ -0,0 +1,41 @@
+﻿module Test1_1
+
+open CodeFSharp.Question1_1
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``No Duplicate returns true`` () =
+    // Arrange
+    let expected = true
+    let input = "abc"
+
+    // Act
+    let actual = AreAllCharactersUnique input
+
+    // Assert
+    Assert.AreEqual (expected, actual)
+
+[<Test>]
+let ``Duplicates returns false`` () =
+    // Arrange
+    let expected = false
+    let input = "aba"
+
+    // Act
+    let actual = AreAllCharactersUnique input
+
+    // Assert
+    Assert.AreEqual (expected, actual)
+
+[<Test>]
+let ``Casing difference returns true`` () =
+    // Arrange
+    let expected = true
+    let input = "Aa"
+
+    // Act
+    let actual = AreAllCharactersUnique input
+
+    // Assert
+    Assert.AreEqual (expected, actual)
