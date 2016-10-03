a219d3e83bdb553eccc3cd2c8db5efbed874549c
Rob Keim <robkeim@gmail.com>
2016-10-03 10:59:07 +0200

    Add Rabin-Karp substring search algorithm

---
+++ CrackingTheCodingInterview/Code/Misc/SubstringSearch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Code
+{
+    // This is an implementation of the Rabin-Karp algorithm which can determine if a string is a substring of another in linear time
+    // with respect to the larger string. Details of the algorithm can be found here:
+    // https://en.wikipedia.org/wiki/Rabin%E2%80%93Karp_algorithm
+    public static class SubstringSearch
+    {
+        public static bool IsSubstring(string toMatch, string toTest)
+        {
+            if (string.IsNullOrEmpty(toMatch))
+            {
+                throw new ArgumentException("Input cannot be null/empty", nameof(toMatch));
+            }
+
+            if (string.IsNullOrEmpty(toTest))
+            {
+                throw new ArgumentException("Input cannot be null/empty", nameof(toTest));
+            }
+
+            // Short circut to save calculation in the case it's impossible to match
+            if (toTest.Length < toMatch.Length)
+            {
+                return false;
+            }
+
+            var hash = 0;
+
+            foreach (var c in toMatch)
+            {
+                hash ^= c;
+            }
+
+            var rollingHash = 0;
+
+            for (int i = 0; i < toTest.Length; i++)
+            {
+                if (i >= toMatch.Length)
+                {
+                    rollingHash ^= toTest[i - toMatch.Length];
+                }
+
+                rollingHash ^= toTest[i];
+
+                if (hash == rollingHash
+                    && i >= toMatch.Length - 1
+                    && toTest.Substring(i - toMatch.Length + 1, toMatch.Length) == toMatch)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Tests/Misc/SubstringSearchTests.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class SubstringSearchTests
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            // Arrange
+            var toMatch = "abc";
+            var toSearch = "_abc_";
+            var expected = true;
+
+            // Act
+            var actual = SubstringSearch.IsSubstring(toMatch, toSearch);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StartsWithMatchTest()
+        {
+            // Arrange
+            var toMatch = "abc";
+            var toSearch = "abc_";
+            var expected = true;
+
+            // Act
+            var actual = SubstringSearch.IsSubstring(toMatch, toSearch);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EndsWithMatchTest()
+        {
+            // Arrange
+            var toMatch = "abc";
+            var toSearch = "_abc";
+            var expected = true;
+
+            // Act
+            var actual = SubstringSearch.IsSubstring(toMatch, toSearch);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MultipleMatchesTest()
+        {
+            // Arrange
+            var toMatch = "abc";
+            var toSearch = "abcabc";
+            var expected = true;
+
+            // Act
+            var actual = SubstringSearch.IsSubstring(toMatch, toSearch);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NoMatchForInvertedStringTest()
+        {
+            // Arrange
+            var toMatch = "abc";
+            var toSearch = "cba";
+            var expected = false;
+
+            // Act
+            var actual = SubstringSearch.IsSubstring(toMatch, toSearch);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NoMatchForNonadjacentStringTest()
+        {
+            // Arrange
+            var toMatch = "abc";
+            var toSearch = "a_b_c";
+            var expected = false;
+
+            // Act
+            var actual = SubstringSearch.IsSubstring(toMatch, toSearch);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NoMatchForDifferentCasingTest()
+        {
+            // Arrange
+            var toMatch = "abc";
+            var toSearch = "ABC";
+            var expected = false;
+
+            // Act
+            var actual = SubstringSearch.IsSubstring(toMatch, toSearch);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ExceptionForNullFirstArgumentTest()
+        {
+            // Arrange
+            string toMatch = null;
+            var toSearch = "abc";
+
+            // Act
+            SubstringSearch.IsSubstring(toMatch, toSearch);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ExceptionForNullSecondArgumentTest()
+        {
+            // Arrange
+            var toMatch = "abc";
+            string toSearch = null;
+
+            // Act
+            SubstringSearch.IsSubstring(toMatch, toSearch);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ExceptionForEmptyStringFirstArgumentTest()
+        {
+            // Arrange
+            var toMatch = string.Empty;
+            var toSearch = "abc";
+
+            // Act
+            SubstringSearch.IsSubstring(toMatch, toSearch);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ExceptionForEmptyStringSecondArgumentTest()
+        {
+            // Arrange
+            var toMatch = "abc";
+            var toSearch = string.Empty;
+
+            // Act
+            SubstringSearch.IsSubstring(toMatch, toSearch);
+        }
+    }
+}
