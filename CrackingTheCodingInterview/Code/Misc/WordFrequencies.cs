55c7d81d7c265a65b6224faac599fdad2143ef9f
Rob Keim <robkeim@gmail.com>
2016-10-12 19:38:33 +0200

    Add nth most occuring word in a list solution

---
+++ CrackingTheCodingInterview/Code/Misc/WordFrequencies.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Code
+{
+    // This class computes the nth most occurring word(s) in a word document. It's been optimized to query multiple times over the same data
+    // set. In the case where you're only interested in querying once there would be no reason to build the final dictionary and store it.
+    //
+    // The complexity of this algorithm is:
+    // Time: O(w) + O(uw log uw) where w is the number of words and uw is the number of unique words
+    // Space: O(uw) where uw is the number of unique words
+    // I've separated the time complexity here because while the n log n sort of the unique words dominates the linear search through all of
+    // the words in the worst case, in practical cases where processing large amounts of text in a given language (ex: a book) there are going
+    // to be many repeated words and the ratio of the number of unique words to the number of words is going to grow logarithmically.
+    public class WordFrequencies
+    {
+        private readonly IReadOnlyDictionary<int, List<string>> nthMostDictionary;
+
+        public WordFrequencies(IEnumerable<string> words)
+        {
+            if (words == null)
+            {
+                throw new ArgumentNullException(nameof(words));
+            }
+
+            var frequencies = BuildFrequencyDictionary(words);
+            nthMostDictionary = BuildNthMostDictionary(frequencies);
+        }
+
+        // There are at least two different ways that could be considered valid orderings here for the following sample case:
+        // A exists 2 times
+        // B exists 2 times
+        // C exists 1 time
+        //
+        // 1. Return a monotonically increasing list
+        //      n = 1 -> { A, B }
+        //      n = 2 -> { C }
+        //
+        // 2. "Olympic style" ordering
+        //      n = 1 -> { A, B }
+        //      n = 3 -> { C }
+        //
+        // There is also the choice of zero or one based indexing. For this problem I've gone with one based "Olympic style" ordering
+        // as it seemed to be more fun and challenging to implement
+        public List<string> GetNthMostOccurring(int nth)
+        {
+            if (nth <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(nth));
+            }
+
+            List<string> result;
+
+            if (!nthMostDictionary.TryGetValue(nth, out result))
+            {
+                result = null;
+            }
+
+            return result;
+        }
+
+        private static Dictionary<string, int> BuildFrequencyDictionary(IEnumerable<string> words)
+        {
+            var frequencies = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                int frequency;
+
+                if (!frequencies.TryGetValue(word, out frequency))
+                {
+                    frequency = 0;
+                }
+
+                frequencies[word] = frequency + 1;
+            }
+
+            return frequencies;
+        }
+
+        private static IReadOnlyDictionary<int, List<string>> BuildNthMostDictionary(Dictionary<string, int> frequencyDictionary)
+        {
+            var results = new Dictionary<int, List<string>>();
+
+            foreach (var frequency in frequencyDictionary)
+            {
+                List<string> values;
+
+                if (!results.TryGetValue(frequency.Value, out values))
+                {
+                    values = new List<string>();
+                    results[frequency.Value] = values;
+                }
+
+                values.Add(frequency.Key);
+            }
+
+            // Use a ConcurrentDictionary here to ensure thread safety for reads later on
+            var finalDictionary = new ConcurrentDictionary<int, List<string>>();
+
+            int index = 1;
+
+            foreach (var values in results.OrderByDescending(kv => kv.Key))
+            {
+                // No need to check the return value as the adds will always be successful since the algorithm ensures uniqueness at this point
+                finalDictionary.TryAdd(index, values.Value.OrderBy(v => v).ToList());
+                index += values.Value.Count;
+            }
+
+            return finalDictionary;
+        }
+    }
+}
---
+++ Tests/Misc/WordFrequenciesTests.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class WordFrequenciesTests
+    {
+        [TestMethod]
+        public void SingleWordTest()
+        {
+            // Arrange
+            var words = StringList("a");
+            var wordFrequencies = new WordFrequencies(words);
+            var expected = StringList("a");
+            var numToFind = 1;
+
+            // Act
+            var actual = wordFrequencies.GetNthMostOccurring(numToFind);
+
+            // Assert
+            AssertListsAreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SingleWordWithMultipleOccurrencesTest()
+        {
+            // Arrange
+            var words = StringList("a", "a");
+            var wordFrequencies = new WordFrequencies(words);
+            var expected = StringList("a");
+            var numToFind = 1;
+
+            // Act
+            var actual = wordFrequencies.GetNthMostOccurring(numToFind);
+
+            // Assert
+            AssertListsAreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TwoWordsMostOccurringTest()
+        {
+            // Arrange
+            var words = StringList("a", "a", "b");
+            var wordFrequencies = new WordFrequencies(words);
+            var expected = StringList("a");
+            var numToFind = 1;
+
+            // Act
+            var actual = wordFrequencies.GetNthMostOccurring(numToFind);
+
+            // Assert
+            AssertListsAreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TwoWordsSecondMostOccurringTest()
+        {
+            // Arrange
+            var words = StringList("a", "a", "b");
+            var wordFrequencies = new WordFrequencies(words);
+            var expected = StringList("b");
+            var numToFind = 2;
+
+            // Act
+            var actual = wordFrequencies.GetNthMostOccurring(numToFind);
+
+            // Assert
+            AssertListsAreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TwoWordsSameOccurrencesAreSortedTest()
+        {
+            // Arrange
+            var words = StringList("a", "a", "b", "b");
+            var wordFrequencies = new WordFrequencies(words);
+            var expected = StringList("a", "b");
+            var numToFind = 1;
+
+            // Act
+            var actual = wordFrequencies.GetNthMostOccurring(numToFind);
+
+            // Assert
+            AssertListsAreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OrderingSkipsValuesTest()
+        {
+            // Arrange
+            var words = StringList("a", "a", "b", "b", "c");
+            var wordFrequencies = new WordFrequencies(words);
+            var expected = StringList("c");
+            var numToFind = 3;
+
+            // Act
+            var actual = wordFrequencies.GetNthMostOccurring(numToFind);
+
+            // Assert
+            AssertListsAreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TooLargeFrequencyReturnsNothingTest()
+        {
+            // Arrange
+            var words = StringList("a");
+            var wordFrequencies = new WordFrequencies(words);
+            List<string> expected = null;
+            var numToFind = 2;
+
+            // Act
+            var actual = wordFrequencies.GetNthMostOccurring(numToFind);
+
+            // Assert
+            AssertListsAreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void InBetweenFrequenciesReturnsNothingTest()
+        {
+            // Arrange
+            var words = StringList("a", "a", "b", "b", "c");
+            var wordFrequencies = new WordFrequencies(words);
+            List<string> expected = null;
+            var numToFind = 2;
+
+            // Act
+            var actual = wordFrequencies.GetNthMostOccurring(numToFind);
+
+            // Assert
+            AssertListsAreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [SuppressMessage("Microsoft.Usage", "CA1806")]
+        public void NullWordsThrowsExceptionTest()
+        {
+            // Act
+            new WordFrequencies(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InvalidNThrowsExceptionTest()
+        {
+            // Arrange
+            var words = StringList("a");
+            var wordFrequencies = new WordFrequencies(words);
+            var numToFind = 0;
+
+            // Act
+            wordFrequencies.GetNthMostOccurring(numToFind);
+        }
+
+        private static List<string> StringList(params string[] values)
+        {
+            return values.ToList();
+        }
+
+        private static void AssertListsAreEqual(List<string> expected, List<string> actual)
+        {
+            if (expected == null && actual == null)
+            {
+                return;
+            }
+
+            Assert.IsTrue(expected != null && actual != null);
+            Assert.IsTrue(expected.Count == actual.Count);
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
