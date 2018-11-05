5e089915cb3c4b5229c0f1ea6123c70186c33745
Rob Keim <robkeim@gmail.com>
2018-11-05 18:13:35 +0700

    Add 4.4 (check balanced)

---
+++ CrackingTheCodingInterview/Code/Chapter 4/Question4_4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Code
+{
+    public static class Question4_4
+    {
+        // 4.4 Check Balanced: Implement a function to check if a binary tree is balanced. For the purpose
+        // of this question, a balanced tree is defined to be a tree such that the heights of the two subtrees
+        // of any node never differ by more than one
+
+        // Space: O(1)
+        // Time: O(N log N)
+        public static bool IsBalanced<T>(BinaryTreeNode<T> root)
+            where T : IEquatable<T>
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            var leftHeight = GetHeight(root.Left);
+            var rightHeight = GetHeight(root.Right);
+
+            return Math.Abs(leftHeight - rightHeight) <= 1
+                && IsBalanced(root.Left)
+                && IsBalanced(root.Right);
+        }
+
+        private static int GetHeight<T>(BinaryTreeNode<T> root)
+            where T : IEquatable<T>
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return 1 + Math.Max(GetHeight(root.Left), GetHeight(root.Right));
+        }
+    }
+}
---
+++ Tests/Chapter 4/Test4_4.cs
@@ -0,0 +1,114 @@
+﻿using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Test4_4
+    {
+        [TestMethod]
+        public void SingleNode_IsBalanced_ReturnsTrue()
+        {
+            // Arrange
+            var root = new BinaryTreeNode<int>(0);
+
+            // Act
+            var result = Question4_4.IsBalanced(root);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void OneLevelDifference_IsBalanced_ReturnsTrue()
+        {
+            /*
+             *   1
+             *  /
+             * 0
+             */
+
+            // Arrange
+            var left = new BinaryTreeNode<int>(0);
+            var root = new BinaryTreeNode<int>(1, left, null);
+
+            // Act
+            var result = Question4_4.IsBalanced(root);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CompleteTree_IsBalanced_ReturnsTrue()
+        {
+            /*
+             *   2
+             *  / \
+             * 0   1
+             */
+
+            // Arrange
+            var left = new BinaryTreeNode<int>(0);
+            var right = new BinaryTreeNode<int>(1);
+            var root = new BinaryTreeNode<int>(2, left, right);
+
+            // Act
+            var result = Question4_4.IsBalanced(root);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void UnbalancedTreeAtRoot_IsBalanced_ReturnsFalse()
+        {
+            /*
+             *     2
+             *    /
+             *   1
+             *  /
+             * 0
+             */
+
+            // Arrange
+            var left2 = new BinaryTreeNode<int>(0);
+            var left = new BinaryTreeNode<int>(1, left2, null);
+            var root = new BinaryTreeNode<int>(2, left, null);
+
+            // Act
+            var result = Question4_4.IsBalanced(root);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void UnbalancedTreeAtLowerLevel_IsBalanced_ReturnsFalse()
+        {
+            /*
+             *       6
+             *      / \
+             *     2   5
+             *    /     \
+             *   1       4
+             *  /         \
+             * 0           3
+             */
+            // Arrange
+            var left3 = new BinaryTreeNode<int>(0);
+            var left2 = new BinaryTreeNode<int>(1, left3, null);
+            var left = new BinaryTreeNode<int>(2, left2, null);
+            var right3 = new BinaryTreeNode<int>(3);
+            var right2 = new BinaryTreeNode<int>(4, null, right3);
+            var right = new BinaryTreeNode<int>(5, null, right2);
+            var root = new BinaryTreeNode<int>(6, left, right);
+
+            // Act
+            var result = Question4_4.IsBalanced(root);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
