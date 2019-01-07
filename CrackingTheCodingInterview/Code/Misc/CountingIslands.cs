c47e620c9dfcfe6df170960ea44ba5c69a76a705
Rob Keim <robkeim@gmail.com>
2019-01-07 12:40:01 +0700

    Add CountingIslands exercise solution

---
+++ CrackingTheCodingInterview/Code/Misc/CountingIslands.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Code
+{
+    public static class CountingIslands
+    {
+        // Given a map indicating land (value 1) and water (value 0) determine the number
+        // of islands that exist on the map. Islands are composed of adjacent land blocks
+        // either directly north, west, east, or south but not diagonally.
+        public static int CountIslands(bool[,] map)
+        {
+            if (map == null)
+            {
+                throw new ArgumentNullException(nameof(map));
+            }
+
+            var numIslands = 0;
+
+            for (int y = 0; y < map.GetLength(1); y++)
+            {
+                for (int x = 0; x < map.GetLength(0); x++)
+                {
+                    if (map[x, y])
+                    {
+                        numIslands++;
+                        MarkVisited(map, x, y);
+                    }
+                }
+            }
+
+            return numIslands;
+        }
+
+        private static void MarkVisited(bool[,] map, int x, int y)
+        {
+            if (x >= 0 && y >= 0 && x < map.GetLength(0) && y < map.GetLength(1)
+                && map[x, y])
+            {
+                map[x, y] = false;
+                MarkVisited(map, x - 1, y);
+                MarkVisited(map, x + 1, y);
+                MarkVisited(map, x, y - 1);
+                MarkVisited(map, x, y + 1);
+            }
+        }
+    }
+}
---
+++ Tests/Misc/CountingIslandsTest.cs
@@ -0,0 +1,112 @@
+﻿using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class CountingIslandsTest
+    {
+        [TestMethod]
+        public void CountIslands_WithNoIslands_ReturnsZero()
+        {
+            // Arrange
+            var map = new bool[2, 2];
+
+            // Act
+            var actual = CountingIslands.CountIslands(map);
+
+            // Assert
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void CountIslands_WithNoIslandsRectangularMap_ReturnsZero()
+        {
+            // Arrange
+            var map = new bool[2, 1];
+
+            // Act
+            var actual = CountingIslands.CountIslands(map);
+
+            // Assert
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void CountIslands_WithOneIsland_ReturnsOne()
+        {
+            // Arrange
+            var map = new bool[2, 2];
+            map[0, 0] = true;
+
+            // Act
+            var actual = CountingIslands.CountIslands(map);
+
+            // Assert
+            Assert.AreEqual(1, actual);
+        }
+
+        [TestMethod]
+        public void CountIslands_WithOneIslandAdjacentSquares_ReturnsOne()
+        {
+            // Arrange
+            var map = new bool[2, 2];
+            map[0, 0] = true;
+            map[0, 1] = true;
+
+            // Act
+            var actual = CountingIslands.CountIslands(map);
+
+            // Assert
+            Assert.AreEqual(1, actual);
+        }
+
+        [TestMethod]
+        public void CountIslands_WithTwoIslands_ReturnsTwo()
+        {
+            // Arrange
+            var map = new bool[3, 3];
+            map[0, 0] = true;
+            map[2, 2] = true;
+
+            // Act
+            var actual = CountingIslands.CountIslands(map);
+
+            // Assert
+            Assert.AreEqual(2, actual);
+        }
+
+        [TestMethod]
+        public void CountIslands_WithTwoIslandsDiagonal_ReturnsTwo()
+        {
+            // Arrange
+            var map = new bool[2, 2];
+            map[0, 0] = true;
+            map[1, 1] = true;
+
+            // Act
+            var actual = CountingIslands.CountIslands(map);
+
+            // Assert
+            Assert.AreEqual(2, actual);
+        }
+
+        [TestMethod]
+        public void CountIslands_WithOneIslandMultipleDirections_ReturnsOne()
+        {
+            // Arrange
+            var map = new bool[3, 3];
+            map[0, 0] = true;
+            map[0, 1] = true;
+            map[0, 2] = true;
+            map[1, 0] = true;
+            map[2, 0] = true;
+
+            // Act
+            var actual = CountingIslands.CountIslands(map);
+
+            // Assert
+            Assert.AreEqual(1, actual);
+        }
+    }
+}
