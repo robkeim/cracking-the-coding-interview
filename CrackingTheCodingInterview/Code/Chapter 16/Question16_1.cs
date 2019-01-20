d2f0025eb46d8e6d2557ea7f3c7ce2a9f4aca273
Rob Keim <robkeim@gmail.com>
2019-01-20 17:16:06 +0700

    Add solution to 16.1 (number swapper)

---
+++ CrackingTheCodingInterview/Code/Chapter 16/Question16_1.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Code
+{
+    public static class Question16_1
+    {
+        // 16.1 Number Swapper: Write a function to swap a number in place (that is, without
+        // temporary variables).
+
+        // Space: O(1)
+        // Time: O(1)
+        [SuppressMessage("Microsoft.Design", "CA1045")]
+        public static void SwapNumbers(ref int first, ref int second)
+        {
+            // first = first ^ second
+            first ^= second;
+
+            // second = second ^ first
+            //        = second ^ (first ^ second)
+            //        = first
+            second ^= first;
+
+            // first = first ^ second
+            //       = (first ^ second) ^ (first)
+            //       = second
+            first ^= second;
+        }
+    }
+}
---
+++ Tests/Chapter 16/Tests16_1.cs
@@ -0,0 +1,54 @@
+﻿using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Tests16_1
+    {
+        [TestMethod]
+        public void SwapNumbers_ReturnsCorrectValue_ForTwoPositiveNumbers()
+        {
+            // Arrange
+            var num1 = 1;
+            var num2 = 2;
+
+            // Act
+            Question16_1.SwapNumbers(ref num1, ref num2);
+
+            // Assert
+            Assert.AreEqual(2, num1);
+            Assert.AreEqual(1, num2);
+        }
+
+        [TestMethod]
+        public void SwapNumbers_ReturnsCorrectValue_ForNegativeNumber()
+        {
+            // Arrange
+            var num1 = -1;
+            var num2 = 2;
+
+            // Act
+            Question16_1.SwapNumbers(ref num1, ref num2);
+
+            // Assert
+            Assert.AreEqual(2, num1);
+            Assert.AreEqual(-1, num2);
+        }
+
+        [TestMethod]
+        public void SwapNumbers_ReturnsCorrectValue_ForNumbersWithSameValue()
+        {
+            // Arrange
+            var num1 = 1;
+            var num2 = 1;
+
+            // Act
+            Question16_1.SwapNumbers(ref num1, ref num2);
+
+            // Assert
+            Assert.AreEqual(1, num1);
+            Assert.AreEqual(1, num2);
+        }
+    }
+}
