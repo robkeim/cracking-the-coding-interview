b72abf0b7dbe82035ac2669c8b94796746929599
Rob Keim <robkeim@gmail.com>
2019-01-20 17:56:50 +0700

    Add solution to 16.2 (word frequencies)

---
+++ CrackingTheCodingInterview/Code/Chapter 16/Question16_2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Code
+{
+    public static class Question16_2
+    {
+        private static Dictionary<string, int> _words;
+
+        // 16.2 Word Frequencies: Design a method to find the frequency of occurances of any
+        // given word in a book. What if we were running this algorithm multiple times?
+
+        // Space: O(1)
+        // Time: O(N)
+        public static int WordFrequency(IEnumerable<string> book, string targetWord)
+        {
+            book = book ?? throw new ArgumentNullException(nameof(book));
+            targetWord = targetWord ?? throw new ArgumentNullException(nameof(targetWord));
+
+            var result = 0;
+
+            foreach (var word in book)
+            {
+                // This treats different cases as different words
+                if (word == targetWord)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        // Space: O(UW) where UW is the number of unique words
+        // Time: O(W) where W is the number of words
+        public static void PreprocessBook(IEnumerable<string> book)
+        {
+            book = book ?? throw new ArgumentNullException(nameof(book));
+
+            _words = new Dictionary<string, int>();
+
+            foreach (var word in book)
+            {
+                // Thsi treats different cases as different words
+                if (!_words.ContainsKey(word))
+                {
+                    _words[word] = 0;
+                }
+
+                _words[word]++;
+            }
+        }
+
+        // This implemention does not support concurrency because of the Dictionary that isn't concurrent and is static
+        // Space: O(1)
+        // Time: O(1)
+        public static int WordFrequencyPreprocessed(string word)
+        {
+            word = word ?? throw new ArgumentNullException(nameof(word));
+            _words = _words ?? throw new InvalidOperationException("The preprocess book method must be called first");
+
+            if (_words.ContainsKey(word))
+            {
+                return _words[word];
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Tests/Chapter 16/Tests16_2.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Tests16_2
+    {
+        [TestMethod]
+        public void WordFrequency_ReturnsFrequency_WithOneWord()
+        {
+            // Arrange
+            var book = new[] { "foo" };
+
+            // Act
+            var result = Question16_2.WordFrequency(book, "foo");
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void WordFrequency_ReturnsFrequency_WithMultipleOccurrences()
+        {
+            // Arrange
+            var book = new[] { "foo", "foo" };
+
+            // Act
+            var result = Question16_2.WordFrequency(book, "foo");
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void WordFrequency_ReturnsFrequency_WithTwoWords()
+        {
+            // Arrange
+            var book = new[] { "foo", "bar" };
+
+            // Act
+            var result = Question16_2.WordFrequency(book, "foo");
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void WordFrequency_ReturnsZero_WithNoOccurrences()
+        {
+            // Arrange
+            var book = new[] { "foo" };
+
+            // Act
+            var result = Question16_2.WordFrequency(book, "bar");
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void WordFrequency_ThrowsArgumentNullException_WhenBookIsNull()
+        {
+            // Act
+            void action() => Question16_2.WordFrequency(null, "foo");
+
+            // Assert
+            TestHelpers.AssertExceptionThrown(action, typeof(ArgumentNullException));
+        }
+
+        [TestMethod]
+        public void WordFrequency_ThrowsArgumentNullException_WhenTargetWordIsNull()
+        {
+            // Arrange
+            var book = new[] { "foo" };
+
+            // Act
+            void action() => Question16_2.WordFrequency(book, null);
+
+            // Assert
+            TestHelpers.AssertExceptionThrown(action, typeof(ArgumentNullException));
+        }
+
+        [TestMethod]
+        public void PreprocessBook_ThrowsArgumentNullException_WhenBookIsNull()
+        {
+            // Act
+            void action() => Question16_2.PreprocessBook(null);
+
+            // Assert
+            TestHelpers.AssertExceptionThrown(action, typeof(ArgumentNullException));
+        }
+
+        [TestMethod]
+        public void WordFrequencyPreprocessed_ReturnsFrequency_WithOneWord()
+        {
+            // Arrange
+            var book = new[] { "foo" };
+            Question16_2.PreprocessBook(book);
+
+            // Act
+            var result = Question16_2.WordFrequencyPreprocessed("foo");
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void WordFrequencyPreprocessed_ReturnsFrequency_WithMultipleOccurrences()
+        {
+            // Arrange
+            var book = new[] { "foo", "foo" };
+            Question16_2.PreprocessBook(book);
+
+            // Act
+            var result = Question16_2.WordFrequencyPreprocessed("foo");
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void WordFrequencyPreprocessed_ReturnsFrequency_WithTwoWords()
+        {
+            // Arrange
+            var book = new[] { "foo", "bar" };
+            Question16_2.PreprocessBook(book);
+
+            // Act
+            var result = Question16_2.WordFrequencyPreprocessed("foo");
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void WordFrequencyPreprocessed_ReturnsZero_WithNoOccurrences()
+        {
+            // Arrange
+            var book = new[] { "foo" };
+            Question16_2.PreprocessBook(book);
+
+            // Act
+            var result = Question16_2.WordFrequencyPreprocessed("bar");
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void WordFrequencyPreprocessed_ThrowsArgumentNullException_WhenTargetWordIsNull()
+        {
+            // Arrange
+            var book = new[] { "foo" };
+            Question16_2.PreprocessBook(book);
+
+            // Act
+            void action() => Question16_2.WordFrequencyPreprocessed(null);
+
+            // Assert
+            TestHelpers.AssertExceptionThrown(action, typeof(ArgumentNullException));
+        }
+    }
+}
