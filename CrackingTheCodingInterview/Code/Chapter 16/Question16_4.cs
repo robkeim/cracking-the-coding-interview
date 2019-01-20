7f0e6e18d0ee08dae1123138b65b812e0e40684e
Rob Keim <robkeim@gmail.com>
2019-01-20 19:15:13 +0700

    Add solution to 16.4 (has winner)

---
+++ CrackingTheCodingInterview/Code/Chapter 16/Question16_4.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Code
+{
+    public static class Question16_4
+    {
+        // 16.4 Tic Tac Win: Design an algorithm to figure out if someone has won a game
+        // of tic-tac-toe.
+
+        // Space: O(1)
+        // Time: O(N^2) where N is the size of one dimension of the board
+        public static bool HasWinner(int?[,] board)
+        {
+            board = board ?? throw new ArgumentNullException(nameof(board));
+
+            const int numDimensions = 3;
+
+            // Check horizontal
+            for (int i = 0; i < numDimensions; i++)
+            {
+                if (!board[0, i].HasValue)
+                {
+                    continue;
+                }
+
+                var value = board[0, i];
+                bool hasWinner = true;
+
+                for (int j = 1; j < numDimensions; j++)
+                {
+                    if (board[j, i] != value)
+                    {
+                        hasWinner = false;
+                        break;
+                    }
+                }
+
+                if (hasWinner)
+                {
+                    return true;
+                }
+            }
+
+            // Check vertical
+            for (int i = 0; i < numDimensions; i++)
+            {
+                if (!board[i, 0].HasValue)
+                {
+                    continue;
+                }
+
+                var value = board[i, 0];
+                bool hasWinner = true;
+
+                for (int j = 1; j < numDimensions; j++)
+                {
+                    if (board[i, j] != value)
+                    {
+                        hasWinner = false;
+                        break;
+                    }
+                }
+
+                if (hasWinner)
+                {
+                    return true;
+                }
+            }
+
+            // Check diagonal top left to bottom right
+            if (board[0, 0].HasValue)
+            {
+                var value = board[0, 0];
+                var hasWinner = true;
+
+                for (int i = 1; i < numDimensions; i++)
+                {
+                    if (board[i, i] != value)
+                    {
+                        hasWinner = false;
+                        break;
+                    }
+                }
+
+                if (hasWinner)
+                {
+                    return true;
+                }
+            }
+
+            // Check diagonal top right to bottom left
+            if (board[numDimensions - 1, 0].HasValue)
+            {
+                var value = board[numDimensions - 1, 0];
+                var hasWinner = true;
+
+                for (int i = 1; i < numDimensions; i++)
+                {
+                    if (board[numDimensions - 1 - i, i] != value)
+                    {
+                        hasWinner = false;
+                        break;
+                    }
+                }
+
+                if (hasWinner)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Tests/Chapter 16/Tests16_4.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Tests16_4
+    {
+        [TestMethod]
+        public void HasWinner_ReturnsFalse_ForEmptyBoard()
+        {
+            // Arrange
+            var board = new int?[3, 3];
+
+            // Act
+            var result = Question16_4.HasWinner(board);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void HasWinner_ReturnsTrue_ForHorizontalWin()
+        {
+            // Arrange
+            var board = new int?[3, 3];
+            board[0, 0] = 1;
+            board[1, 0] = 1;
+            board[2, 0] = 1;
+
+            // Act
+            var result = Question16_4.HasWinner(board);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void HasWinner_ReturnsTrue_ForVerticalWin()
+        {
+            // Arrange
+            var board = new int?[3, 3];
+            board[0, 0] = 1;
+            board[0, 1] = 1;
+            board[0, 2] = 1;
+
+            // Act
+            var result = Question16_4.HasWinner(board);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void HasWinner_ReturnsTrue_ForTopLeftToBottomRightWin()
+        {
+            // Arrange
+            var board = new int?[3, 3];
+            board[0, 0] = 1;
+            board[1, 1] = 1;
+            board[2, 2] = 1;
+
+            // Act
+            var result = Question16_4.HasWinner(board);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void HasWinner_ReturnsTrue_ForTopRightToBottomLeftWin()
+        {
+            // Arrange
+            var board = new int?[3, 3];
+            board[0, 2] = 1;
+            board[1, 1] = 1;
+            board[2, 0] = 1;
+
+            // Act
+            var result = Question16_4.HasWinner(board);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void HasWinner_ReturnsFalse_WithMultiplePlayersInSameRow()
+        {
+            // Arrange
+            var board = new int?[3, 3];
+            board[0, 0] = 1;
+            board[0, 1] = 1;
+            board[0, 2] = 2;
+
+            // Act
+            var result = Question16_4.HasWinner(board);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void HasWinner_ThrowsArgumentInvalidException_WhenBoardIsNull()
+        {
+            // Act
+            void action() => Question16_4.HasWinner(null);
+
+            // Assert
+            TestHelpers.AssertExceptionThrown(action, typeof(ArgumentNullException));
+        }
+    }
+}
